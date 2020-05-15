b96bb869228df5bc5f92e6a6a69667d1b62dbb90
Dustin Blanton <dustinblntn@gmail.com>
2020-05-15 21:30:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+array = [1, 2, 3]
 
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements (array)
+  array.sort do [a, b]
+  array.index[a] <=> array.index[b]
+end
+end
